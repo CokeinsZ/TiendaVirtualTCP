654021711d9291e0ec603cf0e5d1155a830917fd
Stiven Alejandro Carvajal Perez <stivena.carvajalp@autonoma.edu.co>
2025-01-31 17:35:58 -0500

    Estructura inicial

---
+++ vs_client/src/main/java/com/mycompany/vs_client/Vs_client.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vs_client;
+
+/**
+ *
+ * @author Alejandro Carvajal
+ */
+public class Vs_client {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ vs_server/src/main/java/com/mycompany/vs_server/Vs_server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vs_server;
+
+/**
+ *
+ * @author Alejandro Carvajal
+ */
+public class Vs_server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
