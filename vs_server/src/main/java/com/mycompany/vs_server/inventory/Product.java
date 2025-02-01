18c315ff1b9e03c56cf5233362393735b324d6cd
jschicap <jschicap@gmail.com>
2025-02-01 12:24:03 -0500

    Update Product.java

--- vs_server/src/main/java/com/mycompany/vs_server/inventory/Product.java
+++ vs_server/src/main/java/com/mycompany/vs_server/inventory/Product.java
@@ -7,7 +7,37 @@ package com.mycompany.vs_server.inventory;
 /**
  *
  * @author Alejandro Carvajal
+ * @author Jhonatan Chica
  */
+
 public class Product {
+    //The name and id  is private in order to only acces whit set and get 
+    /*name as pructs name 
+    id as products id */
+    private String name;
+    private  String id ;
+
+    public Product(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
     
 }
