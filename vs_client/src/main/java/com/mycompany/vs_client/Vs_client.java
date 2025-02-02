b7e3aa200bd9356952fb4a46b6ab65210571425c
Stiven Alejandro Carvajal Perez <stivena.carvajalp@autonoma.edu.co>
2025-02-02 02:33:13 -0500

    Conexion y Dialogos

--- vs_client/src/main/java/com/mycompany/vs_client/Vs_client.java
+++ vs_client/src/main/java/com/mycompany/vs_client/Vs_client.java
@@ -4,6 +4,16 @@
 
 package com.mycompany.vs_client;
 
+import com.mycompany.vs_client.gui.MainWindow;
+import com.mycompany.vs_client.networkLayer.TCPClient;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Alejandro Carvajal
@@ -11,6 +21,25 @@ package com.mycompany.vs_client;
 public class Vs_client {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Properties p = new Properties();
+        try {
+            p.load(new FileInputStream(new File("properties.properties")));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Vs_client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Vs_client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String sslRoute = p.getProperty("SSL_CERTIFICATE_ROUTE");
+        String sslPassword = p.getProperty("SSL_PASSWORD");
+        System.setProperty("javax.net.ssl.keyStore",sslRoute);
+        System.setProperty("javax.net.ssl.keyStorePassword",sslPassword);
+        System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
+        System.setProperty("javax.net.ssl.trustStore", sslRoute);
+        System.setProperty("javax.net.ssl.trustStorePassword", sslPassword);
+        System.setProperty("javax.net.ssl.trustStoreType", "PKCS12");
+       
+        TCPClient client = new TCPClient("192.168.194.17", 9090);
+        MainWindow mainWindow = new MainWindow(client);
+        mainWindow.setVisible(true);
     }
 }
--- vs_client/src/main/java/com/mycompany/vs_client/gui/MainWindow.java
+++ vs_client/src/main/java/com/mycompany/vs_client/gui/MainWindow.java
@@ -4,18 +4,42 @@
  */
 package com.mycompany.vs_client.gui;
 
+import com.mycompany.vs_client.networkLayer.TCPClient;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Alejandro Carvajal
  */
 public class MainWindow extends javax.swing.JFrame {
 
+    private TCPClient tcpClient;
     /**
      * Creates new form MainWindow
+     * @param tcpClient control the communication with the server
      */
-    public MainWindow() {
+    public MainWindow(TCPClient tcpClient) {
         initComponents();
+        this.tcpClient = tcpClient;
+        
+        showConectionDialog();
     }
+    
+    private void showConectionDialog() {
+        ConnectionDialog conectionDialog = new ConnectionDialog(this, true);
+        conectionDialog.setVisible(true);
+
+        if (conectionDialog.getControl()) {
+            try {
+                tcpClient.connect();
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, "Error al conectar con el servidor: " + ex.getMessage(),
+                        "Error de Conexión", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -26,57 +50,34 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Hello");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(219, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(147, 147, 147))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(73, 73, 73)
+                .addComponent(jLabel1)
+                .addContainerGap(212, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainWindow().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
